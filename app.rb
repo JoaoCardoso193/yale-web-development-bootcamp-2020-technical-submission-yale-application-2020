5702e102a0cd6f49dda32ba6ef4554b632fa172a
Joao Cardoso <joao.cardoso@yale.edu>
2020-04-16 00:52:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
+name = "Joao Cardoso"
+
+age = 21
+
+favorite_food = "sushi"
+
+height_in_inches = 
 
 ## Do not modify below this line!
 
